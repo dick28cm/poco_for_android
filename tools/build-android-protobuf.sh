222b5b336a4be5bd8dead247116a6a40209e9cf1
bigdick 28cm <6366562+dick28cm@users.noreply.github.com>
2021-07-23 23:27:28 +0800

    Update build-android-protobuf.sh

--- tools/build-android-protobuf.sh
+++ tools/build-android-protobuf.sh
@@ -16,16 +16,32 @@
 
 # # read -n1 -p "Press any key to continue..."
 
-set -u
+###############################
+####cmake方式一键编译poco库  目前只支持static静态库
+####
+####
 
+#遇到错误停止
+set -u    
+
+#应用别的
 source ./build-android-common.sh
 
-if [ -z ${version+x} ]; then 
-  version="1.1.1i"
+#判断参数version
+if [[ -z ${ANDROID_NDK_ROOT} ]]; then
+  echo "ANDROID_NDK_ROOT not defined"
+  exit 1
 fi
 
+#判断版本
+if [ -z ${version+x} ]; then 
+  version="3.17.3"
+fi
+
+#初始化日志颜色
 init_log_color
 
+#本地运行路径
 TOOLS_ROOT=$(pwd)
 
 SOURCE="$0"
@@ -39,90 +55,56 @@ pwd_path="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 echo pwd_path=${pwd_path}
 echo TOOLS_ROOT=${TOOLS_ROOT}
 
-# openssl-1.1.0f has a configure bug
-# openssl-1.1.1d has fix configure bug
-LIB_VERSION="OpenSSL_$(echo $version | sed 's/\./_/g')"
-LIB_NAME="openssl-$version"
-LIB_DEST_DIR="${pwd_path}/../output/android/openssl-universal"
+PROTOBUF_NAME="protobuf-${version}"
 
-echo "https://www.openssl.org/source/${LIB_NAME}.tar.gz"
+#判断目录是否存在
+if [ ! -d ${PROTOBUF_NAME} ];then
+    log_info "wget ${PROTOBUF_NAME} to local..."
+    #下载到本地  https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protobuf-cpp-3.17.3.zip
+    rm -rf ${PROTOBUF_NAME}
+    rm -rf protobuf-cpp-${version}.zip
+    wget --no-check-certificate http://github.com/protocolbuffers/protobuf/releases/download/v${version}/protobuf-cpp-${version}.zip
+    unzip "protobuf-cpp-${version}.zip"
+fi
+rm -rf protobuf-cpp-${version}.zip
+cd ${PROTOBUF_NAME}
 
-# https://github.com/openssl/openssl/archive/OpenSSL_1_1_1d.tar.gz
-# https://github.com/openssl/openssl/archive/OpenSSL_1_1_1f.tar.gz
-rm -rf "${LIB_DEST_DIR}" "${LIB_NAME}"
-[ -f "${LIB_NAME}.tar.gz" ] || curl https://www.openssl.org/source/${LIB_NAME}.tar.gz >${LIB_NAME}.tar.gz
 
-set_android_toolchain_bin
-
+#配置cmake
 function configure_make() {
-
     ARCH=$1
     ABI=$2
     ABI_TRIPLE=$3
 
-    log_info "configure $ABI start..."
+    log_info "configure make  protobuf-${ABI} start..."
 
-    if [ -d "${LIB_NAME}" ]; then
-        rm -fr "${LIB_NAME}"
-    fi
-    tar xfz "${LIB_NAME}.tar.gz"
-    pushd .
-    cd "${LIB_NAME}"
+    CMAKE_PATH=$(pwd)/cmake
+    CMAKE_PROJECT_PATH=$(pwd)/cmake-$ABI
+    CMAKE_OUT_PATH="$TOOLS_ROOT/../output/android/poco-$ABI"
 
-    PREFIX_DIR="${pwd_path}/../output/android/openssl-${ABI}"
-    if [ -d "${PREFIX_DIR}" ]; then
-        rm -fr "${PREFIX_DIR}"
-    fi
-    mkdir -p "${PREFIX_DIR}"
+    rm -rf $CMAKE_PROJECT_PATH
 
-    OUTPUT_ROOT=${TOOLS_ROOT}/../output/android/openssl-${ABI}
-    mkdir -p ${OUTPUT_ROOT}/log
-
-    set_android_toolchain "openssl" "${ARCH}" "${ANDROID_API}"
-    set_android_cpu_feature "openssl" "${ARCH}" "${ANDROID_API}"
-
-    export ANDROID_NDK_HOME=${ANDROID_NDK_ROOT}
-    echo ANDROID_NDK_HOME=${ANDROID_NDK_HOME}
-
-    android_printf_global_params "$ARCH" "$ABI" "$ABI_TRIPLE" "$PREFIX_DIR" "$OUTPUT_ROOT"
-
-    if [[ "${ARCH}" == "x86_64" ]]; then
-
-        ./Configure android-x86_64 --prefix="${PREFIX_DIR}"
-
-    elif [[ "${ARCH}" == "x86" ]]; then
-
-        ./Configure android-x86 --prefix="${PREFIX_DIR}"
-
-    elif [[ "${ARCH}" == "arm" ]]; then
-
-        ./Configure android-arm --prefix="${PREFIX_DIR}"
-
-    elif [[ "${ARCH}" == "arm64" ]]; then
-
-        ./Configure android-arm64 --prefix="${PREFIX_DIR}"
-
-    else
-        log_error "not support" && exit 1
-    fi
+    echo  cmake ${CMAKE_PATH} -B${CMAKE_PROJECT_PATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=${CMAKE_OUT_PATH} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${api} -DANDROID_ABI=${ABI} -DBUILD_SHARED_LIBS=OFF
+    cmake ${CMAKE_PATH} -B${CMAKE_PROJECT_PATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=${CMAKE_OUT_PATH} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${api} -DANDROID_ABI=${ABI} -DBUILD_SHARED_LIBS=OFF 
 
     log_info "make $ABI start..."
 
-    make clean >"${OUTPUT_ROOT}/log/${ABI}.log"
-    if make -j$(get_cpu_count) >>"${OUTPUT_ROOT}/log/${ABI}.log" 2>&1; then
-        make install_sw >>"${OUTPUT_ROOT}/log/${ABI}.log" 2>&1
-        make install_ssldirs >>"${OUTPUT_ROOT}/log/${ABI}.log" 2>&1
-    fi
+    cd cmake-$ABI
+    cmake --build . --target libprotobuf install
+    cmake --build . --target libprotobuf-lite install
 
-    popd
+    cd ../
+    rm -rf $CMAKE_PROJECT_PATH
+
+    log_info "make $ABI end..."
 }
 
-log_info "${PLATFORM_TYPE} ${LIB_NAME} start..."
+log_info "begin build-android-protobuf..."
 
 for ((i = 0; i < ${#ARCHS[@]}; i++)); do
     if [[ $# -eq 0 || "$1" == "${ARCHS[i]}" ]]; then
-        configure_make "${ARCHS[i]}" "${ABIS[i]}" "${ARCHS[i]}-linux-android"
+         configure_make "${ARCHS[i]}" "${ABIS[i]}" "${ARCHS[i]}-linux-android"
     fi
 done
 
-log_info "${PLATFORM_TYPE} ${LIB_NAME} end..."
+log_info "build-android-protobuf done..."
